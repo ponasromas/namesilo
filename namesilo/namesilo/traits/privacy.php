d4a35c1a20649a017fbb8daa3aa23e0979409d44
ponasromas <ponasromas@users.noreply.github.com>
2022-06-06 09:56:57 +0300

    Add files via upload

---
+++ namesilo/namesilo/traits/privacy.php
@@ -0,0 +1,82 @@
+<?php
+
+/*
+ * Created on Fri Jun 03 2022
+ *
+ * Copyright (c) 2022 Roman L. https://github.com/ponasromas, https://romas.online
+ *
+ * License: The MIT License (MIT)
+ * Copyright (c) 2022 Roman L.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ * and associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+ * TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+
+declare(strict_types=1);
+
+trait Privacy
+{
+    /*
+    Add WHOIS Privacy to a domain.
+
+    domain: The domain to add privacy to
+    */
+
+    public function addPrivacy(string $domain)
+    {
+        $result = $this->request('addPrivacy', [['domain', $domain]]);
+
+        if ($this->request_successp($result)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /*
+    Remove WHOIS Privacy to a domain.
+
+    domain: The domain to remove privacy for
+    */
+
+    public function removePrivacy(string $domain)
+    {
+        $result = $this->request('removePrivacy', [['domain', $domain]]);
+
+        if ($this->request_successp($result)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /*
+    Custom: check domain privacy status.
+    */
+
+    public function privacyStatus(string $domain)
+    {
+        $domain_info = $this->getDomainInfo($domain);
+        if (!$domain_info) {
+            return false;
+        }
+        $private = strtolower($domain_info['private']);
+        if ($private == 'yes') {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
